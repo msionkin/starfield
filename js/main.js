0496f2e8abff2d2a3af528f07887ebe0e36d7c7d
ionkinms <msionkin@gmail.com>
2016-06-10 01:19:22 +0300

    Added base scene, camera and renderer
    Base scene with black background.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function main() {
+  var scene,
+      camera,
+      renderer,
+      render;
+
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  render = function () {
+    requestAnimationFrame( render );
+
+    renderer.render(scene, camera);
+  };
+}
+
+main();
